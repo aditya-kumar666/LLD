6beb8e73069d5b0995d019d59f927dd799516f5c
Aditya Singh <aditya.kumar666@gmail.com>
2025-08-17 11:07:44 +0530

    updated builder

--- BuilderDesignPattern/Client.cs
+++ BuilderDesignPattern/Client.cs
@@ -7,8 +7,11 @@ public class Client
         Director obj1 = new Director(new EngineeringStudentBuilder());
         Director obj2 = new Director(new MBAStudentBuilder());
 
-        Student engineerStudent = obj1.createStudent();
-        Student mbaStudent = obj2.createStudent();
+
+        //Student engineerStudent = obj1.createStudent();
+        //Student mbaStudent = obj2.createStudent();
+        Student engineerStudent = obj1.createStudent("Aditya", 1, 22);
+        Student mbaStudent = obj2.createStudent("saumya", 2, 23);
 
         Console.WriteLine(engineerStudent.toString());
         Console.WriteLine(mbaStudent.toString());
--- BuilderDesignPattern/Director.cs
+++ BuilderDesignPattern/Director.cs
@@ -9,27 +9,28 @@
             this.studentBuilder = studentBuilder;
         }
 
-        public Student createStudent()
+        public Student createStudent(string name, int rollNo, int age)
         {
-            if(studentBuilder is EngineeringStudentBuilder)
-            {
-                return createEngineeringStudent();
-            }
-            else if(studentBuilder is MBAStudentBuilder)
-            {
-                return createMbaStudent();
-            }
-            return null;
+            return studentBuilder.setName(name).setRollNumber(rollNo).setAge(age).setSubjects().build();
+            //if (studentBuilder is EngineeringStudentBuilder)
+            //{
+            //    return createEngineeringStudent();
+            //}
+            //else if(studentBuilder is MBAStudentBuilder)
+            //{
+            //    return createMbaStudent();
+            //}
+            //return null;
         }
 
-        Student createEngineeringStudent()
-        {
-            return studentBuilder.setName("Aditya").setRollNumber(1).setAge(22).setSubjects().build();
-        }
+        //Student createEngineeringStudent()
+        //{
+        //    return studentBuilder.setName("Aditya").setRollNumber(1).setAge(22).setSubjects().build();
+        //}
 
-        Student createMbaStudent()
-        {
-            return studentBuilder.setName("Saumya").setRollNumber(2).setAge(23).setSubjects().build();
-        }
+        //Student createMbaStudent()
+        //{
+        //    return studentBuilder.setName("Saumya").setRollNumber(2).setAge(23).setSubjects().build();
+        //}
     }
 }
