81e7594c875885aa2d83800f40aeff33b03120ad
Aditya Singh <aditya.kumar666@gmail.com>
2025-08-17 20:57:47 +0530

    updated main

--- AdapterDesignPattern/Client.cs
+++ AdapterDesignPattern/Client.cs
@@ -5,6 +5,9 @@ public class Client
 {
     public static void Main(string[] args)
     {
+        WeightMachine wm = new WeightMachineForBabies();
+        Console.WriteLine(wm.getWeightInPound());
+
         WeightMachineAdapter adapter = new WeightMachineAdapterImpl(new WeightMachineForBabies());
         Console.WriteLine(adapter.getWeightInKg());
     }
